56e8016c3bbed76e5e7cb5c16e0e7393ec76fde3
Ezequiel Sanchez <ezegeek@gmail.com>
2022-06-06 17:20:25 -0300

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -17,4 +17,7 @@ Este snippet comprueba si todos los elementos
 de la matriz son iguales.
 */
 
-<script src="https://gist.github.com/GeekyBear/c8bb12bac1b0eb2b1d776639ba57afb2.js"></script>
+const allEqual = arr => arr.every(val => val === arr[0]);
+
+allEqual ([1, 2, 3, 4, 5, 6]); // false
+allEqual ([1, 1, 1, 1]); // true
